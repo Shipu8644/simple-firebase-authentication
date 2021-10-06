16ac549243e38a4650ced64ea8ad9e93c2f4eb2d
Shipu8644 <shipu.monjurur@gmail.com>
2021-10-06 17:44:06 +0600

    done

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDjxXih-x_EnAPRjTAHcqkllUVVp9jxpY0",
+    authDomain: "simple-firebase-authenti-c7a10.firebaseapp.com",
+    projectId: "simple-firebase-authenti-c7a10",
+    storageBucket: "simple-firebase-authenti-c7a10.appspot.com",
+    messagingSenderId: "957890879206",
+    appId: "1:957890879206:web:77bcc7d5aa623b1054e527"
+};
+export default firebaseConfig;
